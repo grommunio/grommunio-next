a18bcb33537296cbc28c57b83580e1f0b377bcba
Stefan Akie <stefan.akie@grommunio.com>
2023-07-07 14:01:44 +0200

    Implement recursive folder fetching

--- src/api/folders.ts
+++ src/api/folders.ts
@@ -7,12 +7,35 @@ import { PageCollection } from "@microsoft/microsoft-graph-client";
 
 export async function getMailFolders(): Promise<PageCollection> {
   
+  // Get toplevel folders
   const response: PageCollection = await graphClient!
     .api("/me/mailFolders")
     .top(50)  // TODO: This should be configurable
     .count()
     .get();
+
+  // Get recursive childFolders
+  const folders = response.value;
+  for(let i = 0; i < folders.length; i++) {
+    const folder = folders[i];
+    if(folder.childFolderCount > 0) folder["childFolders"] = await getChildFolders(folder.id);
+  }
+  
   return response;
+}
+
+export async function getChildFolders(parentFolderId: string): Promise<MailFolder[]> {
+  const response: PageCollection = await graphClient!
+    .api(`/me/mailFolders/${parentFolderId}/childFolders`)
+    .top(50)  // TODO: This should be unlimited...probably needs more than 1 request if max count is exceeded
+    .get();
+  // Get recursive childFolders
+  const folders = response.value;
+  for(let i = 0; i < folders.length; i++) {
+    const folder = folders[i];
+    if(folder.childFolderCount > 0) folder["childFolders"] = await getChildFolders(folder.id);
+  }
+  return response.value;
 }
 
 export async function postMailFolder(folder: MailFolder): Promise<MailFolder> {
