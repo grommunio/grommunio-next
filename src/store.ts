9ea2eed8460d98e53c9d7363c94f50620f6deb64
Stefan Akie <stefan.akie@grommunio.com>
2024-05-03 15:48:53 +0200

    Fix redux thunk type

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // SPDX-FileCopyrightText: 2020-2023 grommunio GmbH
 
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore, combineReducers, ThunkDispatch } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import logger from 'redux-logger';
@@ -19,6 +19,15 @@ import tasksReducer from './reducers/tasks';
 import { userReducer as meatGrinder } from './reducers/user';
 import alertsReducer from './reducers/alerts';
 import foldersReducer from './reducers/folders';
+
+const getMiddleware = (getDefaultMiddleware: any) => {
+  const middleware = getDefaultMiddleware();
+  // eslint-disable-next-line no-undef
+  if (process.env.NODE_ENV === 'development') {
+    middleware.push(logger);
+  }
+  return middleware;
+}
 
 const rootReducer = combineReducers({
   alerts: alertsReducer,
@@ -38,15 +47,14 @@ const rootReducer = combineReducers({
 // Create redux store
 export const store = configureStore({
   reducer: rootReducer, 
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat([logger])
+  middleware: getMiddleware
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-
-export const useTypeDispatch = () => useDispatch<AppDispatch>();
+export const useTypeDispatch = () => useDispatch<ThunkDispatch<any, any, any>>();
 export const useTypeSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
